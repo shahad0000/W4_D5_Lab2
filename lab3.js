e8eb681a93cc7369ede4e9d11884450347e70c4b
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-08 12:32:26 +0300

    update

--- lab3.js
+++ lab3.js
@@ -10,18 +10,18 @@ let slowLight = document.getElementById("slowLight");
 let goLight = document.getElementById("goLight");
 
 stopButton.onclick = () => {
-    stopLight.style.backgroundColor = "red";
-    slowLight.style.backgroundColor = "#111";
-    goLight.style.backgroundColor = "#111";
+    stopLight.style.background = "radial-gradient(red,rgba(225, 18, 18, 0.72))";
+    slowLight.style.background =  "radial-gradient(#111, #111";;
+    goLight.style.background =  "radial-gradient(#111, #111";
 }
 slowButton.onclick = () => {
-    stopLight.style.backgroundColor = "#111";
-    slowLight.style.backgroundColor = "gold";
-    goLight.style.backgroundColor = "#111";
+    stopLight.style.background =  "radial-gradient(#111, #111";
+    slowLight.style.background = "radial-gradient(gold,rgba(238, 194, 73, 0.72))";
+    goLight.style.background = "radial-gradient(#111, #111";
 }
 
 goButton.onclick = () => {
-    stopLight.style.backgroundColor = "#111";
-    slowLight.style.backgroundColor = "#111";
-    goLight.style.backgroundColor = "limeGreen";
+    stopLight.style.background =  "radial-gradient(#111, #111";
+    slowLight.style.background =  "radial-gradient(#111, #111";
+    goLight.style.background = "radial-gradient(limeGreen, green)";
 }
